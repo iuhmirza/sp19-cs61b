554da48627630530ce1304fd655f0b7aa0a9611a
Ibtisam Mirza <iuhmirza@gmail.com>
2023-02-13 21:51:34 +0000

    Complete lab6

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,35 +1,45 @@
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    int[] elements;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        elements = new int[n];
+        for (int i = 0; i < n; i++) {
+            elements[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex < 0) {
+            throw new Error();
+        }
     }
-
+    public int root(int v1) {
+        int curr = v1;
+        while (parent(curr) >= 0) {
+            curr = parent(curr);
+        }
+        return curr;
+    }
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        return elements[root(v1)];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        return elements[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        int x = root(v1);
+        int y = root(v2);
+        return root(v1) == root(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,7 +48,16 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        if(connected(v1, v2)) {
+            return;
+        }
+        if (sizeOf(v1) > sizeOf(v2)) {
+            elements[root(v2)] += elements[root(v1)];
+            elements[root(v1)] = root(v2);
+        } else {
+            elements[root(v1)] += elements[root(v2)];
+            elements[root(v2)] = root(v1);
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
